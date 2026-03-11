7a4dce85534ba7ce977796da9f86fb5cbe9ff324
yy <yangyong_505@163.com>
2026-03-11 17:47:34 +0800

    更新默认服务器地址

--- src/common.rs
+++ src/common.rs
@@ -1005,7 +1005,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rdp.79035684.xyz:21114".to_owned()
 }
 
 #[inline]
